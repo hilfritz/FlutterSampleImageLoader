2b40c5ab27d43f6cb82e601c10c40d1f8185aff4
Hilfritz Camallere <hilfritz@herdhr.com>
2019-09-18 15:09:00 +0800

    added navigation manager

--- lib/managers/NavigationManager.dart
+++ lib/managers/NavigationManager.dart
@@ -4,6 +4,11 @@ abstract class NavigationManager{
   PresenterComponent presenterComponent;
   void init(PresenterComponent presenterComponent);
 }
+/**
+ * https://www.filledstacks.com/snippet/quick-and-easy-dialogs-in-flutter-with-rf-flutter/
+ * https://medium.com/flutter-community/manager-your-flutter-dialogs-with-a-dialog-manager-1e862529523a
+ * https://medium.com/flutter-community/navigate-without-context-in-flutter-with-a-navigation-service-e6d76e880c1c
+ */
 
 class NavigationManagerImpl implements NavigationManager{
   @override
